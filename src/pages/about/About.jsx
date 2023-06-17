2ce8a58c50e644dc49f26cec4875ace356569296
Mario Junior <mario-cruzeiro@hotmail.com>
2023-06-17 19:40:45 -0300

    Home Done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <main className="section container">
+      <section className="about">
+        <h2 className="section__title">
+          About <span>Me</span>
+        </h2>
+
+        <div className="about__container grid">
+          <div className="about__info">
+            <h3 className="section__subtitle">Personal Infos</h3>
+
+            <ul className="info__list grid"></ul>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Profile from '../../assets/assets/home.jpg'
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+import "./home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <section className="home section grid">
+      <img src={Profile} alt="" className="home__img" />
+
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>Eu sou Mário Júnior.</span> Web designer
+          </h1>
+
+          <p className="home__description">
+              Meu nome é Mário Lúcio Santos Júnior, nascido no dia 07/03/2003, natural de Patrocínio, 
+              e sempre fui fascinado por saber o que acontece por trás dos dispositivos eletrônicos e, com essa curiosidade, cheguei até o curso de 
+              Sistemas de Informação na Universidade Federal de Uberlândia(UFU). Tenho muito apreço pelo respeito, trabalho em equipe, 
+              aprendizagem contínua e, principalmente, dedicação. Acredito que o essencial no ser humano é a sua personalidade, pois habilidades podem ser 
+              adquiridas ao longo do processo, já o caráter do indivíduo é peculiar à ele. Dessa forma, ao ingressar em uma empresa, pretendo fazer um trabalho 
+              eficiente de maneira harmoniosa, ao respeitar a diversidade de pessoas e pensamentos, de modo que o conteúdo final possa gerar um serviço bem feito 
+              e, consequentemente, novos conhecimentos sobre as tecnologias utilizadas.
+          </p>
+
+          <Link to='/about' className='button'>
+            More About Me <span className='button__icon'>
+              <FaArrowRight/>
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color__block"/>
+    </section>
+  );
 }
 
 export default Home

3cf9bdfeee8e56e586361db78ce09ca299eaccd2
Mario Junior <mario-cruzeiro@hotmail.com>
2023-07-05 00:34:36 -0300

    Home page changes - just the title

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,104 @@
 import React from 'react'
+import './contact.css'
+
+import { 
+  FaEnvelopeOpen,
+  FaPhoneSquareAlt,
+  FaFacebookF,
+  FaTwitter,
+  FaYoutube,
+  FaDribbble
+} from 'react-icons/fa'
+
+import { FiSend } from 'react-icons/fi'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact section">
+      <h2 className="section__title">
+        Send Me <span>a Message</span>
+      </h2>
+
+      <div className="contact__container container__grid">
+        <div className="contact__data">
+          <h3 className="contact__title">Don't be shy!</h3>
+          <p className="content__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta repellendus, quisquam ad voluptatum, illum excepturi non placeat repudiandae incidunt voluptates, voluptate cupiditate id dolor suscipit animi odio corrupti culpa ipsum!</p>
+
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className='info__icon'/>
+
+              <div>
+                <span className="info__title">Mail me</span>
+                <span className="info__desc">marioluciosantosjunior@outlook.com.br</span>
+              </div>
+            </div>
+
+            <div className="info__item">
+              <FaPhoneSquareAlt className='info__icon'/>
+              <div>
+                <span className="info__title">Call me</span>
+                <span className="info__desc">+55 034 999461238</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact__socials">
+            <a href="" className="contact__social-link">
+              <FaFacebookF/>
+            </a>
+            <a href="" className="contact__social-link">
+              <FaTwitter/>
+            </a>
+            <a href="" className="contact__social-link">
+              <FaDribbble/>
+            </a>
+            <a href="" className="contact__social-link">
+              <FaYoutube/>
+            </a>
+          </div>
+        
+        </div>
+
+        <form className="contact__form">
+          <div className="form__input-group">
+            <div className="form__input-div">
+              <input 
+              type="text" 
+              placeholder='Your Name'
+              className="form__control" />
+            </div>
+
+            <div className="form__input-div">
+              <input 
+              type="email" 
+              placeholder='Your Email'
+              className="form__control" />                
+            </div>
+
+            <div className="form__input-div">
+              <input 
+              type="text"
+              placeholder='Your Subject'
+              className="form__control" />                
+            </div>
+          </div>
+
+          <div className="form__input-div">
+            <textarea 
+            placeholder='Your Message'
+            className="form__control textarea"></textarea>
+          </div>
+
+          <button className="button">
+            Send Message
+            <span className="button__icon contact__button-icon">
+              <FiSend/>
+            </span>
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,23 +7,13 @@ import "./home.css"
 const Home = () => {
   return (
     <section className="home section grid">
-      <img src={Profile} alt="" className="home__img" />
+      {/*<img src={Profile} alt="" className="home__img" />*/}
 
       <div className="home__content">
         <div className="home__data">
           <h1 className="home__title">
-            <span>Eu sou Mário Júnior.</span> Web designer
+            mar<span>.io</span>
           </h1>
-
-          <p className="home__description">
-              Meu nome é Mário Lúcio Santos Júnior, nascido no dia 07/03/2003, natural de Patrocínio, 
-              e sempre fui fascinado por saber o que acontece por trás dos dispositivos eletrônicos e, com essa curiosidade, cheguei até o curso de 
-              Sistemas de Informação na Universidade Federal de Uberlândia(UFU). Tenho muito apreço pelo respeito, trabalho em equipe, 
-              aprendizagem contínua e, principalmente, dedicação. Acredito que o essencial no ser humano é a sua personalidade, pois habilidades podem ser 
-              adquiridas ao longo do processo, já o caráter do indivíduo é peculiar à ele. Dessa forma, ao ingressar em uma empresa, pretendo fazer um trabalho 
-              eficiente de maneira harmoniosa, ao respeitar a diversidade de pessoas e pensamentos, de modo que o conteúdo final possa gerar um serviço bem feito 
-              e, consequentemente, novos conhecimentos sobre as tecnologias utilizadas.
-          </p>
 
           <Link to='/about' className='button'>
             More About Me <span className='button__icon'>
@@ -33,7 +23,6 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="color__block"/>
     </section>
   );
 }
